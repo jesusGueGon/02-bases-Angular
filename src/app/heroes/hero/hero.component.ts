b7f55baac1f548ac4cdfb158af78971c9cad7ed4
jesusGueGon <jesus.guerrero@bosonit.com>
2023-05-04 15:36:59 +0200

    añadidas funciones a bases angular

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -10,4 +10,33 @@ export class HeroComponent {
   public name: string = "ironman";
   public age: number = 45;
 
+  get capitalizedName(): string {
+
+    return this.name.toUpperCase();
+
+  }
+
+  getHeroDescription(): string {
+
+    return `${this.name} - ${this.age}`;
+
+  }
+
+  changeHero(newName: string): void {
+
+    this.name = newName;
+
+  }
+
+  changeAge(newAge: number): void {
+
+    this.age = newAge;
+
+  }
+
+  resetForm(): void {
+    this.name = "ironman";
+    this.age = 45;
+  }
+
 }
